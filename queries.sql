f54ab6acf675e99d735dc1f2c13fcd27212396dc
Stacey Ruelle <srue23@gmail.com>
2020-02-13 13:37:39 -0600

    Modified queries and added image

--- queries.sql
+++ queries.sql
@@ -72,3 +72,6 @@ GROUP BY last_name
 ORDER BY
 COUNT(last_name) DESC;
 
+-- Evidence in hand, you march into your boss's office and present the visualization. With a sly grin, your boss thanks you for your work. On your way out of the office, you hear the words, "Search your ID number." You look down at your badge to see that your employee ID number is 499942.
+
+SELECT * FROM employees WHERE emp_no = '499942';
